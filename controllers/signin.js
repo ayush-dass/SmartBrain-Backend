e6cf2580c7caaa8f87d8d00344238dc037d11d57
Ayush Das <123637484+ayush-dass@users.noreply.github.com>
2023-08-25 18:44:21 +0530

    Update signin.js

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,20 +1,27 @@
-const handleSignIn = async (req, res, db, bcrypt) => {
+const handleSignIn = (req, res, db, bcrypt) => {
     const { email, password } = req.body;
     if (!email || !password) {
         return res.status(400).json('Incorrect form submission');
     }
-
-    try {
-        const user = await db.select('*').from('users').where('email', '=', email).first();
-
-        if (user && bcrypt.compareSync(password, user.hash)) {
-            res.json(user);
-        } else {
-            res.status(400).json('Wrong Credentials');
-        }
-    } catch (error) {
-        res.status(400).json('Wrong Credentials');
-    }
+    
+    db.select('email', 'hash')
+        .from('login')
+        .where('email', '=', req.body.email)
+        .then(data => {
+            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+            if (isValid) {
+                return db.select('*')
+                    .from('users')
+                    .where('email', '=', email)
+                    .then(user => {
+                        res.json(user[0]);
+                    })
+                    .catch(err => res.status(400).json('Unable to get user'));
+            } else {
+                res.status(400).json('Wrong Credentials');
+            }
+        })
+        .catch(err => res.status(400).json('Wrong Credentials'));
 };
 
 module.exports = {
